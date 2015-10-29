a813cb60c5cf5cadae25350eb345b1682fc93bb0
Jan Rembold <jan.rembold@virtual-identity.com>
2015-10-29 13:35:20 +0100

    added cache to json task

--- gulp/tasks/json.js
+++ gulp/tasks/json.js
@@ -1,11 +1,13 @@
 var gulp     = require('gulp');
 var jsonlint = require('gulp-jsonlint');
+var cached   = require('gulp-cached');
 var config   = require('./../config');
 
 
 gulp.task('lint:json', function () {
 
     return gulp.src( config.global.src + "/_mock/**/*.json")
+        .pipe( cached('json') )
         .pipe( jsonlint() )
         .pipe( jsonlint.reporter() );
 
