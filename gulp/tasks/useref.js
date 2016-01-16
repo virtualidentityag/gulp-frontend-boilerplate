50b2ff5b72ff3f3ef71e6e7e37240450fa8ff322
Jan Rembold <janrembold@googlemail.com>
2016-01-16 14:31:34 +0100

    removed gulp-if added gulp-filter fixed async copying of assets fixed filtering of js/css files for minification

--- gulp/tasks/useref.js
+++ gulp/tasks/useref.js
@@ -1,7 +1,7 @@
 var gulp      = require('gulp');
 var useref    = require('gulp-useref');
 var zetzer    = require('gulp-zetzer');
-var gulpif    = require('gulp-if');
+var filter    = require('gulp-filter');
 var uglify    = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var config    = require('./../config');
@@ -11,17 +11,28 @@ gulp.task('useref', function () {
 
     return gulp.src( config.global.dev + '/*.html' )
         .pipe( useref() )
+        .pipe( filter(['**/*.html']) )
         .pipe( gulp.dest(config.global.dist ) );
 
 });
 
 gulp.task('useref:assets', function () {
 
+    var jsFilter  = filter(['**/*.js'], {restore: true});
+    var cssFilter = filter(['**/*.css'], {restore: true});
+
     return gulp.src( config.global.src + '/resources/_useref.html')
         .pipe( zetzer( config.zetzer ) )
         .pipe( useref() )
-        .pipe( gulpif('*.js', uglify( config.uglify )) )
-        .pipe( gulpif('*.css', minifyCss( config.minifyCss )) )
+
+        .pipe( jsFilter )
+        .pipe( uglify( config.uglify ) )
+        .pipe( jsFilter.restore )
+
+        .pipe( cssFilter )
+        .pipe( minifyCss( config.minifyCss ) )
+        .pipe( cssFilter.restore )
+
         .pipe( gulp.dest( config.global.dist ) );
 
 });
