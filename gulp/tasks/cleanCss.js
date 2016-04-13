de039a8fd76a139325b2dd70f88d428b05e647bd
Alexander Benz <alexander.benz@virtual-identity.com>
2016-04-13 16:18:58 +0200

    updated npm and bower dependencies replaced deprecated gulp-minify-css with gulp-clean-css

--- gulp/tasks/cleanCss.js
+++ gulp/tasks/cleanCss.js
@@ -1,13 +1,13 @@
 var gulp      = require('gulp');
 var size      = require('gulp-size');
-var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var config    = require('./../config');
 
 
 gulp.task('minifyCss:dist', function () {
 
     return gulp.src( config.global.dev + '/resources/css/**/*.css')
-        .pipe( minifyCss( config.minifyCss ) )
+        .pipe( cleanCss( config.cleanCss ) )
         .pipe( size({
             title: 'minified',
             showFiles: true
