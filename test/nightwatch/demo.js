cad1e2214ad164a4def1f8c3383e740ed09f633d
Alexander Benz <alexander.benz@virtual-identity.com>
2016-05-19 16:48:01 +0200

    basic assertion

--- test/nightwatch/demo.js
+++ test/nightwatch/demo.js
@@ -2,6 +2,9 @@ module.exports = {
 	'demo': function(browser) {
 		browser
 			.url(browser.launch_url + '01layout.01default.html')
+			.getText('h1', function(result){
+				this.assert.equal(result.value, 'Default layout structure');
+			})
 			.end();
 	}
 }
